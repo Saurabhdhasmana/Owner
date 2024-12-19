fd0401b82637a5bf053a0870ca860f5494a7668e
Saurabhdhasmana <saurabhdhasmana2019@gmail.com>
2024-12-19 00:56:08 -0800

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const flash = require('connect-flash');
 const session = require('express-session');  
 const cookieParser = require('cookie-parser');
+const RedisStore = require('connect-redis')(session); 
 const ownerRoutes=require("./routes/owner-routes");
 const serviceRoutes=require("./routes/service-routes");
 const portRoutes=require("./routes/portfolio-routes");
@@ -14,9 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-	secret: 'your_secret_key',
-	resave: false,
-	saveUninitialized: true
+  store: new RedisStore({ host: 'localhost', port: 6379 }), // Redis configuration
+  secret: 'your_secret_key',  // Secret key to sign session ID cookie
+  resave: false,
+  saveUninitialized: false,  // Do not save uninitialized session
+  cookie: { secure: false }  // 'secure: false' if you're not using https
 }));
 app.set("view engine", "ejs");
 app.use(flash());
